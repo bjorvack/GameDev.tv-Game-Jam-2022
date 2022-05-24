f1d75a9c5de558ced6054b890a86a5acea0906e5
Bjorn Van Acker <bjorn@sumocoders.be>
2022-05-24 09:27:20 +0200

    Fix wall sticking

--- Assets/Scripts/StateMachines/Player/PlayerMoveState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerMoveState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMoveState : PlayerBaseState
@@ -20,14 +21,32 @@ public class PlayerMoveState : PlayerBaseState
 
     public override void Tick(float deltaTime)
     {
-        ProcessMovement(stateMachine.InputReader.MovementValue);
+        ProcessMovement(stateMachine.InputReader.MovementValue, deltaTime);
     }
     
-    private void ProcessMovement(Vector2 movement)
+    private void ProcessMovement(Vector2 movement, float deltaTime)
     {
         if (movement.x == 0f) { return; }
 
         Debug.Log(movement.x);
+
+        Vector2 feetPosition = new Vector2(
+            stateMachine.transform.position.x,
+            stateMachine.transform.position.y - (stateMachine.GetComponent<CapsuleCollider2D>().size.y / 2f)
+        );
+        float distance = stateMachine.MovementSpeed * deltaTime + (stateMachine.GetComponent<CapsuleCollider2D>().size.x / 2f);
+
+        // Draw raycasts to check if we can move
+        RaycastHit2D[] hits = Physics2D.RaycastAll(feetPosition, new Vector2(Mathf.Sign(movement.x), 0f), distance);
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            {
+                return;
+            }
+
+            Debug.Log(hit.transform.position);
+        }
         
         stateMachine.GetComponent<Rigidbody2D>().velocity = new Vector2(
             stateMachine.MovementSpeed * Mathf.Sign(movement.x),
