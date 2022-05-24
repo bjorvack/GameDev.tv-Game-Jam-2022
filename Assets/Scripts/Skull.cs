ccfeb623771ca9a0aeaf89c5d87b7621535e8a95
Bjorn Van Acker <bjorn@sumocoders.be>
2022-05-24 20:41:42 +0200

    Add follow mechanic to the skulls

--- Assets/Scripts/Skull.cs
+++ Assets/Scripts/Skull.cs
@@ -4,10 +4,46 @@ using UnityEngine;
 
 public class Skull : MonoBehaviour
 {
+    [field: SerializeField] public float MovementSpeed { get; private set; }
+
+    [field: SerializeField] public float DetectionRadius { get; private set; }
+
+    private Vector3 initialPositon;
+
+    private void Start()
+    {
+        initialPositon = transform.position;
+    }
+
+    private void Update()
+    {
+        // If the distance between the player and the skull is less than the detection radius,
+        // then the skull will move towards the player.
+        if (Vector2.Distance(transform.position, FindObjectOfType<PlayerStateMachine>().transform.position) < DetectionRadius)
+        {
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                FindObjectOfType<PlayerStateMachine>().transform.position,
+                MovementSpeed * Time.deltaTime
+            );
+        } else {
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                initialPositon,
+                MovementSpeed * Time.deltaTime
+            );
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player") {
             FindObjectOfType<GameSession>().ProcessPlayerDeath();
         }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, DetectionRadius);
     }
 }
