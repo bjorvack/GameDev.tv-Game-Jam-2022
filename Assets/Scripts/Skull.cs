52f0d1f6051214c1ad2e4d16081e75a918ddfd6e
Bjorn Van Acker <bjorn@sumocoders.be>
2022-05-24 21:47:14 +0200

    Add teleportation doors

--- Assets/Scripts/Skull.cs
+++ Assets/Scripts/Skull.cs
@@ -17,21 +17,46 @@ public class Skull : MonoBehaviour
 
     private void Update()
     {
+        Follow();
+    }
+
+    private void Follow()
+    {
+        Vector2 targetPosition;
         // If the distance between the player and the skull is less than the detection radius,
         // then the skull will move towards the player.
         if (Vector2.Distance(transform.position, FindObjectOfType<PlayerStateMachine>().transform.position) < DetectionRadius)
         {
-            transform.position = Vector2.MoveTowards(
+            targetPosition = Vector2.MoveTowards(
                 transform.position,
                 FindObjectOfType<PlayerStateMachine>().transform.position,
                 MovementSpeed * Time.deltaTime
             );
-        } else {
-            transform.position = Vector2.MoveTowards(
+        }
+        else
+        {
+            targetPosition = Vector2.MoveTowards(
                 transform.position,
                 initialPositon,
                 MovementSpeed * Time.deltaTime
             );
+        }
+
+        FlipSprite(transform.position, targetPosition);
+        transform.position = targetPosition;
+    }
+
+    private void FlipSprite(Vector2 currentPositon, Vector2 targetPositon)
+    {
+        if (currentPositon.x == targetPositon.x) { return; }
+
+        if (currentPositon.x < targetPositon.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
         }
     }
 
